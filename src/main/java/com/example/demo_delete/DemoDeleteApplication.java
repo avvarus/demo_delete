010d3a1149a88f15bfb2f52b7e44e67503fa0103
Avvarus <avvaru79.java@mail.com>
2019-08-06 23:49:48 -0400

    101

---
+++ src/main/java/com/example/demo_delete/DemoDeleteApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_delete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDeleteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDeleteApplication.class, args);
+    }
+
+}
