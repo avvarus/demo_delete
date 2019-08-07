12911c496b61f9e7cd4cc1734eec5c6f5c99ddca
Avvarus <avvaru79.java@mail.com>
2019-08-07 00:08:00 -0400

    102

--- src/main/java/com/example/demo_delete/DemoDeleteApplication.java
+++ src/main/java/com/example/demo_delete/DemoDeleteApplication.java
@@ -2,12 +2,21 @@ package com.example.demo_delete;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoDeleteApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoDeleteApplication.class, args);
+    }
+
+
+    @GetMapping(value = "/he")
+    public String getSer() {
+        return "qwerty";
     }
 
 }
